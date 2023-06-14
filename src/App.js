a26f90d87dde65f2f04fcb4837c74a2cb9c61f63
Mintchoco12 <michaelzhang5654@gmail.com>
2023-06-15 00:08:10 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>Vending machine</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "primary-color": "#222831",
+        "secondary-color": "#393e46",
+        "tertiary-color": "#00adb5",
+        "quaternary-color": "#eeeeee",
+      },
+    },
+  },
+  plugins: [],
+};
