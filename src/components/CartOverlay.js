e30083ef858ce08c0b0b1b3c008d20954d29ef33
Mintchoco12 <michaelzhang5654@gmail.com>
2023-06-27 23:20:59 +0200

    Added items to cart
    + total price
    + clear all button

--- src/components/CartOverlay.js
+++ src/components/CartOverlay.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { cartItems } from "./cartItems.js";
+import { calculateTotalPrice, cartItems, clearCart } from "./cartItems.js";
 
 function CartOverlay({ showOverlay, showMenu }) {
   function generateRandomKey() {
     return Math.random().toString(36).substring(2, 15);
   }
+
+  const totalPrice = calculateTotalPrice();
 
   return (
     <div
@@ -14,7 +16,7 @@ function CartOverlay({ showOverlay, showMenu }) {
     >
       <div className="bg-tertiary-color w-11/12 h-9/10 rounded-lg border-2 border-white z-10">
         <ul className="z-20 grid grid-cols-3 grid-rows-4 w-full h-full">
-        {cartItems.map((snack) => (
+          {cartItems.map((snack) => (
             <li key={generateRandomKey()} className="flex justify-center m-1">
               <img
                 src={snack.image}
@@ -22,7 +24,20 @@ function CartOverlay({ showOverlay, showMenu }) {
                 className="object-contain w-28 rounded-lg"
               />
             </li>
-        ))}
+          ))}
+          <div className="font-bold text-white row-start-4 flex justify-center items-center">
+            <p>Total price: €{totalPrice}</p>
+          </div>
+          <div className="font-bold row-start-4 col-start-3 flex justify-center items-center z-50">
+            <button
+              onClick={() => {
+                clearCart();
+              }}
+              className="bg-red-500 p-3 border-2 border-black rounded-md "
+            >
+              Clear cart
+            </button>
+          </div>
         </ul>
       </div>
       <div
--- src/components/cartItems.js
+++ src/components/cartItems.js
@@ -8,3 +8,16 @@ export const removeFromCart = (snack) => {
   const index = cartItems.findIndex((item) => item.id === snack.id);
   cartItems.splice(index, 1);
 };
+
+export const calculateTotalPrice = () => {
+  let totalPrice = 0;
+  cartItems.forEach((item) => {
+    totalPrice += item.price;
+  });
+  return totalPrice;
+};
+
+export const clearCart = () => {
+  cartItems.splice(0, cartItems.length);
+  console.log(cartItems);
+};
